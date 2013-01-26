1df5b04f2676ffb1f90c0c069882138375e97e0a
jclosure <jclosure@gmail.com>
2013-01-26 03:43:09 -0600

    devise, recurly, signup/subscribe and login working

--- web/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = 'ddf84a84d49241d6d099cb759a3fd72e4d91adfa1aba365001e8bea146e0ca62e46872c6d6e1cde7ea9fda076b0db8c8bfa84b8e66cd8bd641eac5511904f252'
