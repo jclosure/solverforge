62b007a3e580c2dee22632257f8e19025444a18e
jclosure <jclosure@gmail.com>
2013-01-27 22:58:53 -0600

    readded saas. fix repo commit 2

---
+++ saas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRecurlySubscriptionSaas::Application.config.secret_token = '24cf5f04bb015b97e488c54baef8aabbaa610ca7b4abda47dbdbb4d6f68284f45cfc0cd72cd248a90f74208e6df76d3f92aeeee90515fa20a62d8aed779a46f8'
